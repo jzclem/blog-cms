03b3b9063c55b6a6790defe7c338a7e1d08d4b30
jiangzanchang <jiangzanchang@foresee.com.cn>
2021-12-02 09:41:02 +0800

    替换端口

--- vue.config.js
+++ vue.config.js
@@ -7,42 +7,42 @@ module.exports = {
       port: 8081,
       proxy: {
         '/admin': {
-          target: 'http://localhost:3001', //生产 tax-admin
+          target: 'http://localhost:4001', //生产 tax-admin
           ws: false,
           changeOrigin: true
         },
         '/article': {
-          target: 'http://localhost:3001', //生产 tax-admin
+          target: 'http://localhost:4001', //生产 tax-admin
           ws: false,
           changeOrigin: true
         },
         '/category': {
-          target: 'http://localhost:3001', //生产 tax-admin
+          target: 'http://localhost:4001', //生产 tax-admin
           ws: false,
           changeOrigin: true
         },
         '/role': {
-          target: 'http://localhost:3001', //生产 tax-admin
+          target: 'http://localhost:4001', //生产 tax-admin
           ws: false,
           changeOrigin: true
         },
         '/tag': {
-          target: 'http://localhost:3001', //生产 tax-admin
+          target: 'http://localhost:4001', //生产 tax-admin
           ws: false,
           changeOrigin: true
         },
         '/upload': {
-          target: 'http://localhost:3001', //生产 tax-admin
+          target: 'http://localhost:4001', //生产 tax-admin
           ws: false,
           changeOrigin: true
         },
         '/user': {
-          target: 'http://localhost:3001', //生产 tax-admin
+          target: 'http://localhost:4001', //生产 tax-admin
           ws: false,
           changeOrigin: true
         },
         '/menu': {
-          target: 'http://localhost:3001', //生产 tax-admin
+          target: 'http://localhost:4001', //生产 tax-admin
           ws: false,
           changeOrigin: true
         },
